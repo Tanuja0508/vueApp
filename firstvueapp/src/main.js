b65dca12d085023e841d35cfa9d3d7ae3eeb9d68
Tanuja0508 <tanuja0508@gmail.com>
2022-06-21 18:08:54 +0530

    add

--- firstvueapp/src/main.js
+++ firstvueapp/src/main.js
@@ -5,13 +5,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/assets/css/main.css'
 import {createRouter, createWebHistory} from 'vue-router'  
 import {routes} from './route.js'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
-library.add(faSpinner)
-
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas);
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fab);
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(far);
+import { dom } from "@fortawesome/fontawesome-svg-core";
+dom.watch();
   
   const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
   })
-  createApp(App).use(router).mount('#app')
+  createApp(App).use(router).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
